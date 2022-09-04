0e6e5528a4192db4aa0d2df3cbdff9f172eb668b
VegeBun-csj <csj1071285741@gmail.com>
2022-09-04 10:35:32 +0800

    complete zk verifier :rocket:

--- pallets/maci-verifier/src/lib.rs
+++ pallets/maci-verifier/src/lib.rs
@@ -2,14 +2,21 @@
 
 pub use pallet::*;
 
+pub mod types;
+pub mod parser;
+use types::{ProofStr, VkeyStr};
+use parser::{parse_proof, parse_vkey};
+
 #[frame_support::pallet]
 pub mod pallet {
+	use crate::{ProofStr, VkeyStr};
+	use crate::{parse_proof, parse_vkey};
+	use bellman_verifier::{Proof, VerifyingKey, prepare_verifying_key, verify_proof};
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-
-	use sp_std::vec;
-	use sp_std::vec::Vec;
-	use bellman_verifier::{ Proof, VerifyingKey};
+	use sp_std::{vec, vec::Vec};
+	use bls12_381::Bls12;
+	use ff::PrimeField as Fr;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -21,65 +28,92 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::getter(fn proof_store)]
+	pub type Pof<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, ProofStr, OptionQuery>;
 
 
-	// 上链的是Vec<u8>类型
 	#[pallet::storage]
-	#[pallet::getter(fn proof)]
-	pub type Pof<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, Vec<u8>, OptionQuery>;
+	#[pallet::getter(fn vkey_store)]
+	pub type Vkey<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, VkeyStr, OptionQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		SomethingStored(u32, T::AccountId),
+		ProofStored(ProofStr, T::AccountId),
+		VerificationKeyStore(VkeyStr, T::AccountId),
+		VerificationPassed(),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
-		NoneValue,
-		StorageOverflow,
+		NoProof,
+		NoVerificationKey,
+		VerificationFailed,
 	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+
 		#[pallet::weight(0)]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn generate_proof_vkey(
+			origin: OriginFor<T>,
+			proof_a: Vec<u8>,
+			proof_b: Vec<u8>,
+			proof_c: Vec<u8>,
+			vk_alpha1: Vec<u8>,
+			vk_beta_2: Vec<u8>,
+			vk_gamma_2: Vec<u8>,
+			vk_delta_2: Vec<u8>,
+			vk_ic: Vec<Vec<u8>>,
+		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
-
-			<Something<T>>::put(something);
-
-			Self::deposit_event(Event::SomethingStored(something, who));
+			let proof = ProofStr { pi_a: proof_a, pi_b: proof_b, pi_c: proof_c };
+			let vkey = VkeyStr {
+				alpha_1: vk_alpha1, 
+				beta_2: vk_beta_2,
+				gamma_2: vk_gamma_2,
+				delta_2: vk_delta_2,
+				ic: vk_ic,
+			};
+			<Pof<T>>::insert(who.clone(), &proof);
+			<Vkey<T>>::insert(who.clone(),&vkey);
+			Self::deposit_event(Event::<T>::ProofStored(proof, who.clone()));
+			Self::deposit_event(Event::<T>::VerificationKeyStore(vkey, who));
 			Ok(())
 		}
 
 		#[pallet::weight(0)]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		pub fn verifier(origin: OriginFor<T>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
 
-			match <Something<T>>::get() {
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					<Something<T>>::put(new);
-					Ok(())
+			match <Pof<T>>::get(&who) {
+				None => return Err(Error::<T>::NoProof.into()),
+				Some(pof) => {
+					log::info!("{:?}", pof.pi_a);
+					let proof = parse_proof::<Bls12>(pof.clone());
+					log::info!("{:?}", proof.a);
+
+					match <Vkey<T>>::get(&who) {
+						None => return Err(Error::<T>::NoVerificationKey.into()),
+						Some(vkeystr) => {
+							log::info!("{:?}",vkeystr.alpha_1);
+							let vkey = parse_vkey::<Bls12>(vkeystr);
+							log::info!("{:?}",vkey.clone().alpha_g1);
+
+							let pvk =  prepare_verifying_key(&vkey);
+
+							match verify_proof(&pvk, &proof, &[Fr::from_str_vartime("33").unwrap()]) {
+								Ok(()) => Self::deposit_event(Event::<T>::VerificationPassed()),
+								Err(e) => { 
+									log::info!("{:?}", e);
+									()
+								}
+							}
+						}
+					}
 				},
 			}
-		}
 
-		#[pallet::weight(0)]
-		pub fn generate_proof(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResult{
-			let who = ensure_signed(origin)?;
-			<Pof<T>>::insert(who.clone(), proof);
-			Ok(())
-		}
-
-		#[pallet::weight(0)]
-		pub fn verifier(origin: OriginFor<T>, ) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// verify_proof(&pvk, &proof, &[]);
 			Ok(())
 		}
 	}
---
+++ pallets/maci-verifier/src/types.rs
@@ -0,0 +1,18 @@
+use codec::{Encode, Decode};
+use frame_support::RuntimeDebug;
+use sp_std::vec::Vec;
+#[derive(Clone, Encode, Decode, Default, PartialEq, RuntimeDebug, scale_info::TypeInfo)]
+pub struct ProofStr {
+    pub pi_a: Vec<u8>,
+    pub pi_b: Vec<u8>,
+    pub pi_c: Vec<u8>,
+}
+
+#[derive(Clone, Encode, Decode, Default, PartialEq, RuntimeDebug, scale_info::TypeInfo)]
+pub struct VkeyStr {
+    pub alpha_1: Vec<u8>,
+    pub beta_2: Vec<u8>,
+    pub gamma_2: Vec<u8>,
+    pub delta_2: Vec<u8>,
+    pub ic: Vec<Vec<u8>>,
+}
